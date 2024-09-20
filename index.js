1b920983b742bd904dd8ade34cb8b709fb0c6278
GianBo <162351219+Fonchosama@users.noreply.github.com>
2024-09-20 13:23:35 +0200

    cleanup work

--- index.js
+++ index.js
@@ -1,145 +1,149 @@
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const express = require('express');
- morgan = require('morgan');
- fs = require('fs'); 
- path = require('path');
+morgan = require('morgan');
+fs = require('fs');
+path = require('path');
+const Models = require('./models.js');
+const Movies = Models.Movie;
+const Users = Models.User;
 
 const app = express();
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(morgan('combined', {stream: accessLogStream}));
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {
+  flags: 'a',
+});
+app.use(morgan('combined', { stream: accessLogStream }));
 
-let topMovies = [
-    {
-      title: 'Pulp Fiction',
-      author: 'Quentin Tarantino'
-    },
-    {
-      title: 'Rear Window',
-      author: 'Alfred Hitchcock'
-    },
-    {
-      title: 'Citizen Kane',
-      author: 'Orson Welles'
-    },
-    {
-        title: 'Fight Club',
-        author: 'David Fincher'
-    },
-    {
-        title: 'Inception',
-        author: 'Christopher Nolan'
-    },
-    {
-        title: 'Forrest Gump',
-        author: 'Robert Zemeckis'
-    },
-    {
-        title: 'The Matrix',
-        author: 'Wachowski Brothers'
-    },
-    {
-        title: 'Se7en',
-        author: 'David Fincher'
-    },
-    {
-        title: 'Terminator 2: Judgment Day',
-        author: 'James Cameron'
-    },
-    {
-        title: 'Gladiator',
-        author: 'Ridley Scott'
-    }
-  ];
-
-  let topBooks = [
-    {
-      title: 'Harry Potter and the Sorcerer\'s Stone',
-      author: 'J.K. Rowling'
-    },
-    {
-      title: 'Lord of the Rings',
-      author: 'J.R.R. Tolkien'
-    },
-    {
-      title: 'Twilight',
-      author: 'Stephanie Meyer'
-    }
-  ];
-  
-  
-  // GET requests
-  app.get('/', (req, res) => {
-    res.send('Welcome to my Movie Data Base!');
-  });
-  
-  app.get('/documentation', (req, res) => {                  
-    res.sendFile('public/documentation.html', { root: __dirname });
-  });
-
-  app.get('/secreturl', (req, res) => {
-    res.send('This is a secret url with super top-secret content.');
-  });
-  
-  app.get('/movies', (req, res) => {
-    res.json(topMovies);
-  });
-
-  app.get('/Books', (req, res) => {
-    res.json(topBooks);
-  });
-
-  app.get('/movies', (req, res) => {
-    res.send("Successful GET request returning data of all movies");
-  });
-
-  app.get("/movies/:name", (req, res) =>{
-    res.send("Successful GET request returning data of an specific movie")
-  });
-
-  app.get("/genres/:name", (req, res) =>{
-    res.send("Successful GET request returning data about a genre")
-  });
-
-  app.get("/directors/:name", (req, res) =>{
-    res.send("Successful GET request returning data about a director")
-  });
-
- 
-  //POST request 
-
-  app.post("/users/register", (req, res) =>{
-    res.send("Successful POST request to register a new user")
-  });
-
- //PUT requests 
-  
- app.put("/users/:username", (req, res) =>{
-  res.send("Successful PUT request to update the username")
+// Connecting to the DB
+mongoose.connect('mongodb://127.0.0.1:27017/gianflixdb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.put("/users/:username/favorites", (req, res) =>{
-  res.send("Successful PUT request to add a movie to user favorites list")
+// GET requests
+app.get('/', (req, res) => {
+  res.send('Welcome to GianFlix!');
 });
 
-  //DELETE request 
+app.get('/documentation', (req, res) => {
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
 
-  app.delete("/users/:username/favorites", (req, res) =>{
-    res.send("Successful DELETE request to delete a movie from the list of favorites")
-  });
+// get all movies
+app.get('/movies', async (req, res) => {
+  await Movies.find()
+    .then((movies) => {
+      res.status(200).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
-  app.delete("/users/:username", (req, res) =>{
-    res.send("Successful DELETE request to delete an existing user")
-  });
+// get a movie by id
+app.get('/movies/:movieID', async (req, res) => {
+  await Movies.findOne({ _id: req.params.movieID })
+    .then((movie) => {
+      res.status(200).json(movie);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
-  // Serve static files from the "public" directory
+// Get all users
+app.get('/users', async (req, res) => {
+  await Users.find()
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// Get a user by username
+app.get('/users/:Username', async (req, res) => {
+  await Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// Create a new user (used in user signup)
+app.post('/users', async (req, res) => {
+  await Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + 'already exists');
+      } else {
+        Users.create({
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday,
+        })
+          .then((user) => {
+            res.status(201).json(user);
+          })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
+});
+
+// Update a User information
+app.put('/users/:Username', (req, res) => {
+  res.send('Successful PUT request to update the username');
+});
+
+// Add a movie to user's favorite list
+app.put('/users/:Username/movies/:movieID', (req, res) => {
+  res.send('Successful PUT request to add a movie to user favorites list');
+});
+
+// Remove a movie from user's favorite list
+app.delete('/users/:Username/movies/:movieID', (req, res) => {
+  res.send(
+    'Successful DELETE request to delete a movie from the list of favorites'
+  );
+});
+
+// Delete a User
+app.delete('/users/:Username', (req, res) => {
+  Users.deleteOne({ Username: req.params.Username })
+    .then(() => res.status(200).send('Successfully delete the user.'))
+    .catch((e) => {
+      console.error(e);
+      res.status(500).send('Failed to delete the user.');
+    });
+});
+
+// Serve static files from the "public" directory
 app.use(express.static('public'));
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
-  
-  // listen for requests
-  app.listen(8080, () => {
-    console.log('Your app is listening on port 8080.');
-  });
+
+// listen for requests
+app.listen(8080, () => {
+  console.log('GianFlix is listening on port 8080.');
+});
