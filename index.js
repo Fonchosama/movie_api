314e58ed4a23dfef92b0414d914f09808f1c5dec
GianBo <162351219+Fonchosama@users.noreply.github.com>
2024-10-03 08:42:40 +0200

    hashed password change
    Changes on the lines 209 & 214

--- index.js
+++ index.js
@@ -206,12 +206,14 @@ app.put(
       //condition start
       return res.status(400).send('Permission denied');
     } // end of condition
+
+    let hashedPassword = Users.hashPassword(req.body.Password);
     await Users.findOneAndUpdate(
       { Username: req.params.Username },
       {
         $set: {
           Username: req.body.Username,
-          Password: req.body.Password,
+          Password: hashedPassword,
           Email: req.body.Email,
           Birthday: req.body.Birthday,
         },
