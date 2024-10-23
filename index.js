b67933969ef58a878d2607a157f68ecbf2fe41b0
GianBo <162351219+Fonchosama@users.noreply.github.com>
2024-10-23 17:02:03 +0200

    auth enabled /movies

--- index.js
+++ index.js
@@ -48,16 +48,20 @@ app.get('/documentation', (req, res) => {
 }); //working
 
 // get all movies
-app.get('/movies', async (req, res) => {
-  await Movies.find()
-    .then((movies) => {
-      res.status(200).json(movies);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    });
-}); //working
+app.get(
+  '/movies',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res) => {
+    await Movies.find()
+      .then((movies) => {
+        res.status(200).json(movies);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      });
+  }
+); //working
 
 // get a movies by title
 app.get(
