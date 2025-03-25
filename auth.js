2a0118aa369d56ef04ff3ba0121ee1812e859931
GianBo <gianbo251@gmail.com>
2025-03-25 19:02:17 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,3 +1,7 @@
+/**
+ * Secret key used to sign JWT tokens.
+ * @constant {string}
+ */
 const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the JWTStrategy
 
 const jwt = require('jsonwebtoken'),
@@ -6,6 +10,13 @@ const jwt = require('jsonwebtoken'),
 require('./passport'); // Your local passport file
 
 let generateJWTToken = (user) => {
+  /**
+ * Generates a JWT token for a given user.
+ * @function
+ * @param {Object} user - The user object.
+ * @param {string} user.Username - The username to encode in the JWT.
+ * @returns {string} The signed JWT token.
+ */
   return jwt.sign(user, jwtSecret, {
     subject: user.Username, // This is the username you’re encoding in the JWT
     expiresIn: '7d', // This specifies that the token will expire in 7 days
@@ -14,8 +25,21 @@ let generateJWTToken = (user) => {
 };
 
 //POST login.
-
+/**
+ * Defines the login route using Passport authentication.
+ * @function
+ * @param {Object} router - The Express router instance.
+ */
 module.exports = (router) => {
+    /**
+   * POST /login
+   * @name Login
+   * @function
+   * @memberof module:routes
+   * @param {Object} req - Express request object.
+   * @param {Object} res - Express response object.
+   * @returns {Object} JSON object containing the user and JWT token.
+   */
   router.post('/login', (req, res) => {
     passport.authenticate('local', { session: false }, (error, user, info) => {
       if (error || !user) {
